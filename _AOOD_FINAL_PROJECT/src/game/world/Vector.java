90a25d2ddf765202dc9f8b5aa9f05e56f3b362ee
zcarr8445 <zcarr8445@omhmdi1dtde003.inst.hcpss.org>
2016-05-17 14:49:54 -0400

    Vector (for velocity!)

---
+++ _AOOD_FINAL_PROJECT/src/game/world/Vector.java
@@ -0,0 +1,28 @@
+package game.world;
+
+public class Vector {
+
+	private double x, y;
+
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getX() {
+		return this.x;
+	}
+
+	public double getY() {
+		return this.y;
+	}
+
+}
