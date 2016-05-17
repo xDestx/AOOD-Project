dc92f3e1b4f85dc1100f1eb94fd830576a08038c
ptian9707 <ptian9707@OMHB45C42634CC.inst.hcpss.org>
2016-05-17 11:34:23 -0400

    player stufff

--- _AOOD_FINAL_PROJECT/src/game/entity/Player.java
+++ _AOOD_FINAL_PROJECT/src/game/entity/Player.java
@@ -2,12 +2,30 @@ package game.entity;
 
 import java.awt.Graphics;
 
+import game.Location;
+
 public class Player extends Entity
 {
-
+	public Location location;
+	private int health, stength;
+	
+	public Player()
+	{
+	}
+	
+	public Location getLocation()
+	{
+		return location;
+	}
+	
     public void render(Graphics g, int xo, int yo)
     {
     }
+    
+    public void attack()
+    {
+    }
+    
     
     
 }
