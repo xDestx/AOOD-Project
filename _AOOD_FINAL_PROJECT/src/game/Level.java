a979cea2e4068a4a439145ad0f639bcda33b45cd
ptian9707 <ptian9707@OMHB45C42634CC.inst.hcpss.org>
2016-05-17 11:59:07 -0400

    just spaces

--- _AOOD_FINAL_PROJECT/src/game/Level.java
+++ _AOOD_FINAL_PROJECT/src/game/Level.java
@@ -6,21 +6,25 @@ import java.io.Serializable;
 import java.awt.Rectangle;
 
 public class Level implements Serializable{
+	
     private ArrayList<Collidable> listOfCollidables;
     private int id;
+    
     public Level(int idNumber){
         listOfCollidables = new ArrayList<Collidable>();
         id = idNumber;
     }
+    
     public int getId(){
         return id;
     }
+    
     public ArrayList<Collidable> getListOfCollidables(){
         return listOfCollidables;
     }
     
     public void render(Rectangle bounds)
-    {
-           
+    { 
     }
+    
 }
