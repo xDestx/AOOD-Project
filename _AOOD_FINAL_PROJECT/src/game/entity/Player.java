67a4e8aa6854ffa912640f411b69ccb65e6e13f7
Zachary Carr <destroyer7712@gmail.com>
2016-05-18 21:35:00 -0400

    dying

--- _AOOD_FINAL_PROJECT/src/game/entity/Player.java
+++ _AOOD_FINAL_PROJECT/src/game/entity/Player.java
@@ -124,6 +124,7 @@ public class Player extends Entity {
 				 * Check if colliding x / y then move
 				 */
 				//Check x
+				//k
 				double vx = velocity.getX();
 				double vy = velocity.getY();
 				if(c.getBounds().getMaxX() > getBounds().getMinX() || c.getBounds().getMinX() < getBounds().getMaxX())
