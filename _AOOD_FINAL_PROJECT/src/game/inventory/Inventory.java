56c16ffe9a71a79cd7dfbd96c8e60f6b5fbd5475
zcarr8445 <zcarr8445@OMHB45C4248089.inst.hcpss.org>
2016-05-31 11:46:39 -0400

    item inv

--- _AOOD_FINAL_PROJECT/src/game/inventory/Inventory.java
+++ _AOOD_FINAL_PROJECT/src/game/inventory/Inventory.java
@@ -1,19 +1,46 @@
 package game.inventory;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import game.GFrame;
+import game.Renderable;
+import game.world.Location;
+
 public class Inventory {
 	
-	public boolean isOpen;
+	private boolean isOpen;
+	private Item[] items;
 
 	public Inventory() {
 		isOpen = false;
-		/*
-		 * Will do stuff later
-		 */
+		items = new Item[20];
+		for(int i = 0; i < 20; i++)
+		{
+			items[i] = null;
+		}
 	}
 	
 	public boolean isOpen()
 	{
 		return isOpen;
+	}
+	/*
+	 * Fixed position
+	 */
+	public void render(Graphics g)
+	{
+		Color last = g.getColor();
+		g.setColor(Color.black);
+		g.fillRect(10, 10, GFrame.WIDTH-20, GFrame.HEIGHT-20);
+		g.setColor(Color.gray);
+		g.drawRect(20,20, GFrame.WIDTH-40, GFrame.HEIGHT-40);
+		for (int i = 0; i < 5; i++)
+		{
+			double xo = 20 + ((i+1)*((GFrame.WIDTH-40)/5.0));
+			g.drawLine((int)xo, 20, (int)xo, GFrame.HEIGHT-20);
+		}
 	}
 
 }
