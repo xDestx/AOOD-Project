77a415db78a90a884f7a4819801a4efc602aa412
Zachary Carr <destroyer7712@gmail.com>
2016-05-31 14:47:07 -0400

    Slight progress

--- _AOOD_FINAL_PROJECT/src/game/inventory/Inventory.java
+++ _AOOD_FINAL_PROJECT/src/game/inventory/Inventory.java
@@ -32,14 +32,26 @@ public class Inventory {
 	public void render(Graphics g)
 	{
 		Color last = g.getColor();
-		g.setColor(Color.black);
+		g.setColor(new Color(0,0,0,127));
 		g.fillRect(10, 10, GFrame.WIDTH-20, GFrame.HEIGHT-20);
 		g.setColor(Color.gray);
 		g.drawRect(20,20, GFrame.WIDTH-40, GFrame.HEIGHT-40);
+		//Vertical lines
 		for (int i = 0; i < 5; i++)
 		{
 			double xo = 20 + ((i+1)*((GFrame.WIDTH-40)/5.0));
 			g.drawLine((int)xo, 20, (int)xo, GFrame.HEIGHT-20);
+		}
+		//Horizontal lines
+		for(int i = 0; i < 4; i++)
+		{
+			double yo = 20 + ((i+1)*((GFrame.HEIGHT-40)/4.0));
+			g.drawLine(20, (int)yo, GFrame.WIDTH-20, (int)yo);
+		}
+		//items
+		for (int i = 0; i < 20; i++)
+		{
+			//Do things here!
 		}
 	}
 
