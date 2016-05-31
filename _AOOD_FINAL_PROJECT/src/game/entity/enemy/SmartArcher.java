2bdc17f179b4205822073694f8f7433a72efcdef
Phil <Phil@Lenovo-PC>
2016-05-31 11:47:18 -0400

    Smart Archer

--- _AOOD_FINAL_PROJECT/src/game/entity/enemy/SmartArcher.java
+++ _AOOD_FINAL_PROJECT/src/game/entity/enemy/SmartArcher.java
@@ -6,6 +6,7 @@ import game.world.Location;
 import game.world.Vector;
 
 public class SmartArcher extends Archer {
+	
 
 	@Override
 	public void attack()
@@ -14,7 +15,7 @@ public class SmartArcher extends Archer {
 		{
 			if(Game.getCurrentGame().getPlayer().getBounds().intersects(getAttackBounds()))
 			{
-				//Game.getCurrentGame().getPlayer().wasHit(strength);
+				/*//Game.getCurrentGame().getPlayer().wasHit(strength);
 				Location l = Game.getCurrentGame().getPlayer().getCenterLocation();
 				Location firingLocation = new Location(l);
 				double div = (l.distance(getCenterLocation())/(Game.TICK*10));
@@ -22,6 +23,7 @@ public class SmartArcher extends Archer {
 				{
 					div = 3;
 				}
+				double div = 2;
 				System.out.println(div + " !");
 				firingLocation.setX(Game.getCurrentGame().getPlayer().getVelocity().getX()*(Game.TICK * div));
 				firingLocation.setY(Game.getCurrentGame().getPlayer().getVelocity().getY()*(Game.TICK * div));
@@ -34,11 +36,39 @@ public class SmartArcher extends Archer {
 				/*
 				 * Proportional
 				 */
-				double xv = xp * 10;
+				/*double xv = xp * 10;
 				double yv = yp * 10;
 				//Total V (Hypotenuse) = 10
 				Vector v = new Vector(xv,yv);
-				Game.getCurrentGame().getLevel().addProjectile(new Projectile(new Location(getCenterLocation()), v, this, strength));//Total speed of 10 p/t (pixels/tick)
+				Game.getCurrentGame().getLevel().addProjectile(new Projectile(new Location(getCenterLocation()), v, this, strength));//Total speed of 10 p/t (pixels/tick)*/
+				
+				//shoots normally if player not moving;
+				double hypo = 0;
+				Location l = Game.getCurrentGame().getPlayer().getCenterLocation();
+				double lx = l.getX()-getCenterLocation().getX();
+				double ly = l.getY()-getCenterLocation().getY();
+				hypo = Math.sqrt(Math.pow(lx, 2)+Math.pow(ly, 2));
+				double xp = lx/hypo;
+				double yp = ly/hypo;
+				double xv = xp * 10;
+				double yv = yp * 10;
+				
+				//finds predicted location of player
+				Vector player = Game.getCurrentGame().getPlayer().getVelocity();
+				double xd = l.getX() - getCenterLocation().getX();
+				double yd = l.getY() - getCenterLocation().getY();
+				double predictlx = (player.getX() * 30);
+				double predictly = (player.getY() * 30);
+				
+				double finalHypo = Math.sqrt(Math.pow(lx, 2)+Math.pow(ly, 2));
+				//double d = Math.acos(Math.abs(l.getX()-getLocation().getX())/hypo);
+				double predictxp = predictlx/finalHypo;
+				double predictyp = predictly/finalHypo;
+				double predictxv = predictxp * 10;
+				double predictyv = predictyp * 10;
+				Vector v = new Vector(xv + predictxv,yv + predictyv);
+				//Vector v = new Vector(xv, yv);
+				launchProjectile(v,strength);
 			}
 			//addAnimation(new PlayerHitAnimation(this));
 			cooldownTicks = 0;
