27cc3443783419d3b85fec3ad9babb3de3f525e0
zcarr8445 <zcarr8445@OMHB45C4248089.inst.hcpss.org>
2016-05-31 11:19:24 -0400

    They work on enemies

--- _AOOD_FINAL_PROJECT/src/game/entity/neutral/Projectile.java
+++ _AOOD_FINAL_PROJECT/src/game/entity/neutral/Projectile.java
@@ -8,6 +8,7 @@ import game.world.Vector;
 import game.Game;
 import game.entity.Entity;
 import game.entity.Player;
+import game.entity.enemy.Enemy;
 import game.util.Task;
 import game.world.Location;
 
@@ -17,17 +18,15 @@ public class Projectile extends Entity {
 	private Entity owner;
 	private int damage;
 	/*
-	 * No collision
-	 * Why?
-	 * Lazy.
+	 * No collision Why? Lazy.
 	 */
-	
+
 	public Projectile(Location l, Vector v, Entity owner, int damage) {
 		super(l);
 		this.owner = owner;
 		this.damage = damage;
-		this.velocity=v;
-		setBounds(new Rectangle((int)l.getX(),(int)l.getY(),20,20));
+		this.velocity = v;
+		setBounds(new Rectangle((int) l.getX(), (int) l.getY(), 20, 20));
 		/*
 		 * Default life of 5 seconds
 		 */
@@ -35,23 +34,21 @@ public class Projectile extends Entity {
 
 	@Override
 	public void render(Graphics g, int xo, int yo) {
-		//Render circle
+		// Render circle
 		Color last = g.getColor();
 		g.setColor(Color.blue);
 		g.fillOval(xo, yo, 20, 20);
 		g.setColor(last);
 	}
-	
-	public void moveX(double x)
-	{
-		this.getLocation().setX(this.getLocation().getX()+x);
+
+	public void moveX(double x) {
+		this.getLocation().setX(this.getLocation().getX() + x);
 		this.getBounds().setLocation(this.getLocation().toPoint());
 	}
-	
-	public void moveY(double y)
-	{
 
-		this.getLocation().setY(this.getLocation().getY()+y);
+	public void moveY(double y) {
+
+		this.getLocation().setY(this.getLocation().getY() + y);
 		this.getBounds().setLocation(this.getLocation().toPoint());
 	}
 
@@ -63,20 +60,32 @@ public class Projectile extends Entity {
 		 * Check collision later
 		 */
 		Player p = Game.getCurrentGame().getPlayer();
-		if(p.getBounds().intersects(getBounds()))
-		{
-			if(owner != p)
-			{
+		if (p.getBounds().intersects(getBounds())) {
+			if (owner != p) {
 				p.wasHit(damage, owner);
 				final Projectile bye = this;
-				Game.getCurrentGame().addTask(new Task(0)
-						{
+				Game.getCurrentGame().addTask(new Task(0) {
 					@Override
-					public void run()
-					{
+					public void run() {
 						Game.getCurrentGame().getLevel().removeProjectile(bye);
 					}
-						});
+				});
+				return;
+			}
+		}
+		for (Enemy e : Game.getCurrentGame().getLevel().getEnemies()) {
+			if (e.getBounds().intersects(getBounds())) {
+				if (owner != e) {
+					e.wasHit(damage, owner);
+					final Projectile bye = this;
+					Game.getCurrentGame().addTask(new Task(0) {
+						@Override
+						public void run() {
+							Game.getCurrentGame().getLevel().removeProjectile(bye);
+						}
+					});
+					return;
+				}
 			}
 		}
 	}
